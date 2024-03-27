6d4526695f482b878bd029a05e2232258af00dab
henrikvtcodes <commits@henrikvt.com>
2024-03-26 22:24:38 -0400

    add tailwind and stuff

---
+++ site/tailwind.config.ts
@@ -0,0 +1,23 @@
+import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter Tight Variable",
+          "Inter Tight",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        mono: [
+          "JetBrains Mono Variable",
+          "JetBrains Mono",
+          ...defaultTheme.fontFamily.mono,
+        ],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
