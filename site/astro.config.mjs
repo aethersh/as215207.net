916d5e11d0e6aff1544b5c29fa65752d0c88ee69
henrikvtcodes <commits@henrikvt.com>
2024-03-29 15:07:32 -0400

    add svelte for later additions

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import prefetch from "@astrojs/prefetch";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), prefetch()]
+  integrations: [tailwind(), sitemap(), prefetch(), svelte()]
 });
