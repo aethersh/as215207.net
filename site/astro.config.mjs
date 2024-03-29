13505acd77c18ba57b3459d10ac6e19e2f61bdcb
henrikvtcodes <commits@henrikvt.com>
2024-03-29 15:08:28 -0400

    add "site" config option

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
@@ -7,5 +7,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), prefetch(), svelte()]
+  site: "https://as215207.net",
+  integrations: [tailwind(), sitemap(), prefetch(), svelte()],
 });
